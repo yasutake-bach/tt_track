9314ec4e96fd964d0f487c46b8c2abfea502f24c
satorunakamatsu <satoru.nakamatsu@gmail.com>
2023-03-14 21:04:57 +0900

    都道府県セレクタでボタンを押した時にフォーム全体POSTの回避、fetchでpref_cdを送信、Djangoでfilterしてレスポンスする

--- station_data/views.py
+++ station_data/views.py
@@ -11,7 +11,13 @@ from .models import Station, Line, ConnectedStation, Company
 
 def station_view(request):
     print(request.GET)
-    response = serialize('geojson', Station.objects.all(),
-    geometry_field='point',
-          )
+    queryset = Station.objects.all()
+
+    if 'pref_cd' in request.GET:
+        queryset = queryset.filter(pref_cd=request.GET['pref_cd'])
+
+    if 'add' in request.GET:
+        queryset = queryset.filter(add__contains=request.GET['add'])
+    
+    response = serialize('geojson', queryset, geometry_field='point')
     return HttpResponse(response, content_type='application/geo+json')
